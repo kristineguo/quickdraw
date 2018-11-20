dcdde10f8a48366202aae8d382140c7b5dcd4aa1
kristineguo <kguo98@stanford.edu>
2018-11-19 22:47:49 -0700

    fixed up knn_pp

--- k_nearest_neighbor.py
+++ k_nearest_neighbor.py
@@ -1,5 +1,7 @@
 import numpy as np
 from math import log10, floor
+from collections import defaultdict
+import os
 
 def round_to_1(x):
     return round(x, -int(floor(log10(abs(x)))))
@@ -20,36 +22,23 @@ def compute_distances(X1, X2):
     assert X1.shape[1] == X2.shape[1]
 
     dists = np.zeros((M, N))
-    #print("Computing Distances")
-    #print(X1.shape)
-    #print(X2.shape)
-    #dists = np.sqrt(((X2 - X1[:, np.newaxis])**2).sum(axis=2))
     
     print("Computing Distances")
-    benchmark = int(round_to_1(M)//10)
-    for i in range(len(X1)):
-      for j in range(len(X2)):
-        dists[i,j] = np.linalg.norm(X1[i] - X2[j])
-      if i % benchmark == 0:
-        print(str(i//benchmark)+"0% complete")
+    if os.path.isfile("dists.npy"):
+        dists = np.load("dists.npy")
+    else:
+        benchmark = int(round_to_1(M)//10)
+        for i in range(len(X1)):
+          for j in range(len(X2)):
+            dists[i,j] = np.linalg.norm(X1[i] - X2[j])
+          if i % benchmark == 0:
+            print(str(i//benchmark)+"0% complete")
+        np.save("dists", dists)
+    
     print("Distances Computed")
-
-
-    '''
-    a_2 = np.sum(np.square(X1), axis = 1)
-    ab2 = 2*np.dot(X1, X2.T)
-    b_2 = np.sum(np.square(X2), axis = 1)
-    dists = np.sqrt(np.abs(a_2[:,np.newaxis]-ab2+b_2[np.newaxis,:]))
-    '''
-
-    #print("Distances computed!")
-
-    assert dists.shape == (M, N), "dists should have shape (M, N), got %s" % dists.shape
-
     return dists
 
-
-def predict_labels(dists, y_train, k=1):
+def predict_labels(dists, y_train, y_val, k=1):
     """Given a matrix of distances `dists` between test points and training points,
     predict a label for each test point based on the `k` nearest neighbors.
 
@@ -62,60 +51,12 @@ def predict_labels(dists, y_train, k=1):
                 test data, where y[i] is the predicted label for the test point X[i].
     """
     num_test, num_train = dists.shape
-    y_pred = np.zeros(num_test, dtype=np.int)
+    y_pred = defaultdict(list)
 
     for i in range(num_test):
         closest_y = y_train[np.argpartition(dists[i], k)[:k]]
         occur = np.bincount(closest_y)
-        max_occur = np.max(occur)
-        y_pred[i] = np.min(np.where(occur == max_occur))
-        #y_pred[i] = np.argmax(np.bincount(closest_y))
-
+        top = sorted(enumerate(occur), key=lambda a: a[1], reverse=True)
+        y_pred[y_val[i]].append([cat for cat, _ in top[:3]])
+    
     return y_pred
-
-
-def split_folds(X_train, y_train, num_folds):
-    """Split up the training data into `num_folds` folds.
-
-    The goal of the functions is to return training sets (features and labels) along with
-    corresponding validation sets. In each fold, the validation set will represent (1/num_folds)
-    of the data while the training set represent (num_folds-1)/num_folds.
-    If num_folds=5, this corresponds to a 80% / 20% split.
-
-    For instance, if X_train = [0, 1, 2, 3, 4, 5], and we want three folds, the output will be:
-        X_trains = [[2, 3, 4, 5],
-                    [0, 1, 4, 5],
-                    [0, 1, 2, 3]]
-        X_vals = [[0, 1],
-                  [2, 3],
-                  [4, 5]]
-
-    Args:
-        X_train: numpy array of shape (N, D) containing N examples with D features each
-        y_train: numpy array of shape (N,) containing the label of each example
-        num_folds: number of folds to split the data into
-
-    jeturns:
-        X_trains: numpy array of shape (num_folds, train_size * (num_folds-1) / num_folds, D)
-        y_trains: numpy array of shape (num_folds, train_size * (num_folds-1) / num_folds)
-        X_vals: numpy array of shape (num_folds, train_size / num_folds, D)
-        y_vals: numpy array of shape (num_folds, train_size / num_folds)
-
-    """
-    assert X_train.shape[0] == y_train.shape[0]
-
-    validation_size = X_train.shape[0] // num_folds
-    training_size = X_train.shape[0] - validation_size
-
-    X_trains = np.zeros((num_folds, training_size, X_train.shape[1]))
-    y_trains = np.zeros((num_folds, training_size), dtype=np.int)
-    X_vals = np.zeros((num_folds, validation_size, X_train.shape[1]))
-    y_vals = np.zeros((num_folds, validation_size), dtype=np.int)
-
-    for i in range(num_folds):
-        X_trains[i] = np.concatenate((X_train[:i*validation_size], X_train[(i+1)*validation_size:]))
-        X_vals[i] = X_train[i*validation_size:(i+1)*validation_size]
-        y_trains[i] = np.concatenate((y_train[:i*validation_size], y_train[(i+1)*validation_size:]))
-        y_vals[i] = y_train[i*validation_size:(i+1)*validation_size]
-
-    return X_trains, y_trains, X_vals, y_vals
--- knn_pp.py
+++ knn_pp.py
@@ -28,56 +28,12 @@ def load_centroids():
     return centroids, labels
 
 if __name__ == "__main__":
-
-    k = 12
+    k = 10
     x_train, y_train = load_centroids()
-    #y_train = np.repeat(np.arange(len(x_train)/5), 5)
     x_val, y_val = load_dataset("val")
     y_train = y_train.astype(int)
     y_val = y_val.astype(int)
-    #print(y_train)
-    #print(labels)
-    #assert np.allclose(y_train, labels)
-    #print(x_train.shape)
-    #print(y_train.shape)
-    #print(x_val.shape)
-    #print(y_val.shape)
 
     dists = compute_distances(x_val, x_train)
-
-    y_test_pred = predict_labels(dists, y_train, k)
-
-    num_test = y_val.shape[0]
-    num_correct = np.sum(y_test_pred == y_val)
-    accuracy = float(num_correct) / num_test
-    print('Got %d / %d correct => accuracy: %f' % (num_correct, num_test, accuracy))
-    '''
-    k = 100
-    accuracies = []
-    for i in range(1, k):
-      x_train, y_train = load_centroids()
-      x_val, y_val = load_dataset("val")
-      y_train = y_train.astype(int)
-      y_val = y_val.astype(int)
-      #print(x_train.shape)
-      #print(y_train.shape)
-      #print(x_val.shape)
-      #print(y_val.shape)
-
-      dists = compute_distances(x_val, x_train)
-
-      y_test_pred = predict_labels(dists, y_train, i)
-
-      num_test = y_val.shape[0]
-      num_correct = np.sum(y_test_pred == y_val)
-      accuracy = float(num_correct) / num_test
-      print(i)
-      print('Got %d / %d correct => accuracy: %f' % (num_correct, num_test, accuracy))
-      accuracies.append(accuracy)
-    plt.figure()
-    plt.plot(range(k-1), accuracies)
-    plt.title("Optimal k value")
-    plt.xlabel("k value")
-    plt.ylabel("accuracy")
-    plt.savefig("best_k.png")
-    '''
+    pred = predict_labels(dists, y_train, y_val, k)
+    compute_scores(pred)
