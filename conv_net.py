289809cfc9ed3c0dfb42bdbe2ba7f8a705ee4cbb
James WoMa <jaywoma@stanford.edu>
2018-12-07 19:24:31 -0800

    Changed git ignore and conv_net, we have 48% accuracy

--- conv_net.py
+++ conv_net.py
@@ -8,7 +8,7 @@ LEARNING_RATE = 1e-3
 NUM_CLASSES = 345
 BATCH_SIZE = 32
 
-NUM_TRAINING = 20000
+NUM_TRAINING = 352956
 NUM_VALIDATION = 20000
 
 def get_accuracy(pred, y):
@@ -33,12 +33,12 @@ def run_epoch(model, sess, x, y, is_training, lr=None):
         for i in range(cur_indices.shape[0]):
             pred[cur_indices[i]] = np.argmax(predictions[i])
         avg_loss += loss/num_batches
-        
+
         if b % 1000 == 999:
             print("completed %d batches" % (b + 1))
     print("average loss:", avg_loss)
     print("accuracy:", get_accuracy(pred, y))
-    
+
 def preprocess(x, y, mean=None, std=None):
     x = x.astype(float)
     y = y.astype(int)
@@ -54,13 +54,13 @@ def preprocess(x, y, mean=None, std=None):
 def run(model, sess):
     x_train, y_train = load_dataset("train")
     x_val, y_val = load_dataset("val")
-    
+
     indices = np.arange(x_train.shape[0])
     np.random.shuffle(indices)
 
     x_train, y_train, mean, std = preprocess(x_train[indices[:NUM_TRAINING]], y_train[indices[:NUM_TRAINING]])
     x_val, y_val, _, _ = preprocess(x_val[:NUM_VALIDATION], y_val[:NUM_VALIDATION], mean, std)
-    
+
     for epoch in range(NUM_EPOCHS):
         print("Running on Epoch %d..." % epoch)
         print("Training...")
